135de969e9a734d9607fc24cd696767974b54405
Jeremy Grosser <jeremy@synack.me>
2024-09-11 15:14:18 -0700

    use internal pullup to avoid errata E9 in test

--- test_arm_m33/src/test.adb
+++ test_arm_m33/src/test.adb
@@ -33,8 +33,8 @@ begin
        OD         => True,    --  Output disable
        IE         => True,    --  Input enable
        DRIVE      => 0,       --  No drive
-       PUE        => False,   --  No pull up
-       PDE        => True,    --  Pull down (open drain)
+       PUE        => True,    --  Pull up
+       PDE        => False    --  No pull down
        SCHMITT    => True,    --  Schmitt trigger
        SLEWFAST   => False);  --  Slew control
    NVIC.ICPR (RP2350.Interrupts.IO_IRQ_BANK0) := True;
